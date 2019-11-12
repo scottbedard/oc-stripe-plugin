5b03c26374b3a723d47a6014ed6407b05713d7c2
Scott Bedard <scottbedard90@gmail.com>
2019-11-11 19:20:12 -0700

    initial plugin scaffolding

---
+++ Plugin.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Bedard\Saas;
+
+use System\Classes\PluginBase;
+
+/**
+ * Saas Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Plugin dependencies.
+     */
+    public $require = [
+        'RainLab.User',
+    ];
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+    }
+    
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'author'        => 'Scott Bedard',
+            'description'   => 'Software as a service with Stripe',
+            'icon'          => 'icon-cc-stripe',
+            'name'          => 'Saas',
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return [];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return [];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return [];
+    }
+}
