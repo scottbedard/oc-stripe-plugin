c4344bca67788cc5f65451627d0ae7871e5dfd33
Scott Bedard <scottbedard90@gmail.com>
2019-11-30 20:51:45 -0700

    try setting config if undefined

--- tests/PluginTestCase.php
+++ tests/PluginTestCase.php
@@ -79,6 +79,12 @@ abstract class PluginTestCase extends BasePluginTestCase
         // set rainlab.user min password length
         Config::set('rainlab.user::minPasswordLength', 8);
 
+        // set stripe keys
+        if (!Config::get('services.stripe.key') || !Config::get('services.stripe.secret')) {
+            Config::set('services.stripe.key', env('STRIPE_KEY'));
+            Config::set('services.stripe.secret', env('STRIPE_SECRET'));
+        }
+
         // register model factories
         App::singleton(Factory::class, function ($app) {
             $faker = $app->make(Generator::class);
